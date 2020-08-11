7d0dd3e2ac21869e7289c12f1b166051fa31a6d6
KrlosGarcia <ccgarciadavid@hotmail.com>
2020-08-10 20:38:12 -0500

    Tarea del Corredor

--- src/Corredor/Main.java
+++ src/Corredor/Main.java
@@ -11,53 +11,55 @@ public class Main {
         Scanner in = new Scanner(System.in);
         Corredor e1 = new Corredor();
         int correr,recarga;
-        String run = null;
+        String abc = null;
         
         
        System.out.print("Ingrese la energia total del corredor: ");
        e1.setEnergia(in.nextInt());
        do{
-           System.out.println("\nIngrese el número de veces que el competidor correrá: ");
+           System.out.println("\nDigite las veces que el competidor correrá: ");
            correr = in.nextInt();
-           
-       
-            System.out.println("\nEl competidor cempezará a correr...");
             
-            for(int i =0 ; i < correr ; i ++){
+       
+            System.out.println("\nEl competidor empezará a correr...");
+            
+            for(int i=0;i<correr;i++){
                     e1.correr();
                     e1.verificarEnergia();
             }
             
-            System.out.print("\nLa energia del corredor es: " + e1.getEnergia()); 
+            System.out.print("\nLa energía del corredor es: "+e1.getEnergia()); 
             
             
             if(e1.getEnergia() == 0){
                 System.out.println();
                 e1.energiaCero();
-                System.out.println("\nRecargando energia...");
-                System.out.println("Indique el número veces que el competidor entrenará: "); recarga = in.nextInt();
-                for(int i = 0 ; i < recarga ; i ++){
+                System.out.println("\nRecargando energía...");
+                System.out.println("Indique las veces que el competidor entrenará: "); recarga = in.nextInt();
+                for(int i=0;i<recarga;i++){
                     e1.recargarEnergia(10);
                     e1.entrenar();
                 }
-                System.out.print("\nLa energia del corredor es: " + e1.getEnergia());
+                System.out.print("\nLa energía del corredor es: "+e1.getEnergia());
             }
            
             else{
-                System.out.println("\nRecargando energia...");
-                System.out.println("Indique el número veces que el competidor entrenará: "); recarga = in.nextInt();
-                for(int i = 0 ; i < recarga ; i ++){
+                System.out.println("\nRecargando energía...");
+                System.out.println("Indique las veces que el competidor entrenará: "); recarga = in.nextInt();
+                for(int i=0;i<recarga;i++){
                     e1.recargarEnergia(10);
                     e1.entrenar();
                 }
-                System.out.print("\nLa energia del corredor es: " + e1.getEnergia());
+                System.out.print("\nLa energía del corredor es: "+e1.getEnergia());
             }
             
-            System.out.println("\n\nDesea que el competidor vuelva a correr? [Si/No]");
-            run = in.next();
+            System.out.println("\n\nQuiere que el competidor vuelva a correr? (S/N)");
+            abc = in.next();
             System.out.println("\n\n");
-        }while("s".equals(run) || "S".equals(run) || "si".equals(run) || "SI".equals(run) || "Si".equals(run));    
-               
+        }while("s".equals(abc) || "S".equals(abc) || "si".equals(abc) || "SI".equals(abc) || "Si".equals(abc));    
+        
+       
+        
     }
     
 }
